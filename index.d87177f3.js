7308f1f15f783aaa32500b0e07746c200ce73fd7
HelenaGingembre <99600719+HelenaGingembre@users.noreply.github.com>
2022-09-19 11:00:05 +0000

    Deploying to gh-pages from @ HelenaGingembre/test-js@b2b6837cec3231396dca6a6a14aed64dcb5187bf 🚀

---
+++ index.d87177f3.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.d87177f3.js.map
